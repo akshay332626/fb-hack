b3d268c2558696e25a1be122f7f1ed0e694c0618
Graham Holborn <graham@mellenger.com>
2012-09-20 14:54:53 -0700

    Facebook login work

--- sites/all/themes/hc/templates/page--front.tpl.php
+++ sites/all/themes/hc/templates/page--front.tpl.php
@@ -55,6 +55,70 @@
     <?php endif; ?>
 
     <?php print render($page['content']); ?>
+    
+    
+    
+    
+    
+    <?php  /* FACEBOOK CONNECT */ 
+        
+
+   $app_id = "140232946122321";
+   $app_secret = "a3f0f7dc94ca89e92dc513eb541e48dc";
+   $my_url = "http://hide.mellenger.com";
+
+?>
+
+<div id="fb-root"></div>
+    <script>
+      // Load the SDK Asynchronously
+      (function(d){
+         var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement('script'); js.id = id; js.async = true;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         ref.parentNode.insertBefore(js, ref);
+       }(document));
+
+      // Init the SDK upon load
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '140232946122321', // App ID
+          channelUrl : '//'+window.location.hostname+'/channel', // Path to your Channel File
+          status     : true, // check login status
+          cookie     : true, // enable cookies to allow the server to access the session
+          xfbml      : true  // parse XFBML
+        });
+
+        // listen for and handle auth.statusChange events
+        FB.Event.subscribe('auth.statusChange', function(response) {
+          if (response.authResponse) {
+              console.log('test1');
+          } else {
+            // user has not auth'd your app, or is not logged into Facebook
+            console.log('test2');
+          }
+        });
+
+        // respond to clicks on the login and logout links
+        $('.fb-login-button').click(function(){
+           // FB.login();
+        });
+
+
+      } 
+    </script>
+
+    <div class="fb-login-button" scope="publish_actions">
+        Connect with Facebook
+      </div>
+
+    
+    
+    
+    
+    
+    
   </div>
   <!-- Main content area -->
 

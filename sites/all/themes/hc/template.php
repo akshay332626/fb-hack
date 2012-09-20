5cd5c156cb288bf8ab3923f4473a7d7a64cd76f6
Andrew Mellenger <andrew@mellenger.com>
2012-09-20 13:55:23 -0700

    more modules and theme

---
+++ sites/all/themes/hc/template.php
@@ -0,0 +1,13 @@
+<?php
+
+	/*
+	Preprocess function to determine number of columns for main content area
+	*/
+	function hc_preprocess_page(&$variables) {
+		if ( !empty($variables['page']['left']) && !empty($variables['page']['right']) )
+			$variables['main_columns'] = 'six';
+		else if ( !empty($variables['page']['left']) || !empty($variables['page']['right']) )
+			$variables['main_columns'] = 'nine';
+		else if ( empty($variables['page']['left']) && empty($variables['page']['right']) )
+			$variables['main_columns'] = 'twelve';
+	}
---
+++ sites/all/themes/hc/templates/html.tpl.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7 <?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://opengraphprotocol.org/schema/" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8 <?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://opengraphprotocol.org/schema/" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9 <?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://opengraphprotocol.org/schema/" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js <?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://opengraphprotocol.org/schema/" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>> <!--<![endif]-->
+
+<html class="no-js <?php print $classes; ?>">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# hidecache: http://ogp.me/ns/fb/hidecache#">
+  <meta charset="utf-8" />
+  <meta property="fb:app_id" content="140232946122321" />
+  <?php echo $head; ?>
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title><?php echo $head_title; ?></title>
+
+  <!-- Included CSS Files -->
+  <?php echo $styles; ?>
+
+  <script src="<?php echo $directory; ?>/js/modernizr.foundation.js"></script>
+
+  <!-- IE Fix for HTML5 Tags -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Included JS Files -->
+  <?php echo $scripts; ?>
+
+</head>
+
+<body <?php print $attributes;?>>
+	<?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
